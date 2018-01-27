efbda00322fcb1a6eaf0898548307eaa0af43d7c
Petra Barus <petra.barus@gmail.com>
2018-01-27 12:16:59 +0700

    Add skeleton.

---
+++ lambda/handler.py
@@ -0,0 +1,27 @@
+"""
+Lambda handler to receive SNS event and then run ECS RunTask
+"""
+
+import json
+
+def generate_coverage(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
