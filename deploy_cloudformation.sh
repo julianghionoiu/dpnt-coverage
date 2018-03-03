2dd703fc89bf0f540076b8642413c7e7b9ad792d
Petra Barus <petra.barus@gmail.com>
2018-03-03 23:59:05 +0700

    Reorganize template and add cloudformation for ECS.

---
+++ deploy_cloudformation.sh
@@ -0,0 +1,7 @@
+aws \
+    --region $DPTN_COVERAGE_AWS_REGION \
+    --profile $DPTN_COVERAGE_AWS_PROFILE \
+     cloudformation \
+        create-stack \
+            --stack-name dpnt-coverage \
+            --template-body file://cloudformation.ym
