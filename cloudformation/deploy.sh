91be9c504fe7eded93dafb400a58311e20c54040
Petra Barus <petra.barus@gmail.com>
2018-03-10 07:13:17 +0700

    - Add docker definition for java and sourcecode record - Add cloudformation for build. - Add buildspec skeleton.

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -1,7 +1,11 @@
+################################################################################
+# 
+################################################################################
 aws \
     --region $DPTN_COVERAGE_AWS_REGION \
     --profile $DPTN_COVERAGE_AWS_PROFILE \
      cloudformation \
         create-stack \
             --stack-name dpnt-coverage \
-            --template-body file://definition.yml
+            --template-body file://definition.yml \
+             --capabilities CAPABILITY_IAM
