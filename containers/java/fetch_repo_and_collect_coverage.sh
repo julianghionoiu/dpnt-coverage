da0697ed16139f6e6a1dad6e046fda11c2ec350c
mani <sadhak001@gmail.com>
2018-05-09 16:47:29 +0100

    Removed redundant WORKING_DIR variable and also updated other aspects, fixed the name of the script file to get_coverage...

--- containers/java/fetch_repo_and_collect_coverage.sh
+++ containers/java/fetch_repo_and_collect_coverage.sh
@@ -4,18 +4,17 @@ set -e
 set -u
 set -o pipefail
 
-workingDir=$1
-repo=$2
-tag=$3
-challengeId=$4
+REPO=$1
+TAG=$2
+CHALLENGE_ID=$3
 
-echo "Switching to $HOME and cloning repo: ${repo}"
-cd ${workingDir} && git clone ${repo} tdl-runner
-cd ${workingDir}/tdl-runner
+echo "Switching to $HOME and cloning repo: ${REPO}"
+git clone ${REPO} tdl-runner
+cd tdl-runner
 
-echo "Switching to tag: ${tag}"
-git checkout ${tag}
-git pull origin ${tag}
+echo "Switching to tag: ${TAG}"
+git checkout ${TAG}
+git pull origin ${TAG}
 
-echo "Running script to retrieve the code coverage for ${challengeId} for the repo ${repo}"
-./getCodeCoverageFor.sh ${challengeId}
+echo "Running script to retrieve the code coverage for ${CHALLENGE_ID} for the repo ${REPO}"
+./get_coverage_for_challenge.sh ${CHALLENGE_ID}
