a2abeccfd8bb708ddc3a17c2057e0515776529c8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-05-21 16:37:02 +0100

    Improve coverage script so that it checks for the ENV variables existence

--- containers/hmmm/fetch_repo_and_collect_coverage.sh
+++ containers/hmmm/fetch_repo_and_collect_coverage.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 set -u
 set -o pipefail
 
-REPO=$1
-TAG=$2
-CHALLENGE_ID=$3
+# To check if an ENV variable exists, we dereference the input string $1 -> !1
+# then then we attempt parameter expansion with the "+x" string
+# it should return an empty string if ENV not defined
+# Reference: https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
+function ensure_env {
+    if [ -z "${!1+x}" ]; then echo "Environment variable $1 not set"; exit 1; fi
+}
 
-echo "Switching to $HOME and cloning repo: ${REPO}"
+ensure_env "REPO"
+ensure_env "TAG"
+ensure_env "CHALLENGE_ID"
+
+# Clone and switch to TAG
 git clone ${REPO} tdl-runner
 cd tdl-runner
-
-echo "Switching to tag: ${TAG}"
 git checkout ${TAG}
 git pull origin ${TAG}
 
-echo "Running script to retrieve the code coverage for ${CHALLENGE_ID} for the repo ${REPO}"
+# Run the coverage
+echo  "~~~~~~ START run external script ~~~~~~" > /dev/null
 ./get_coverage_for_challenge.sh ${CHALLENGE_ID}
+echo "~~~~~~ STOP run external script ~~~~~~" > /dev/null
+
+# Publish to Queue
