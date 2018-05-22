0db9c7b1cbf7785b2adafa516438a2965a69d237
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-05-22 20:35:53 +0100

    Download the SRCS exporter and add to Dockerfile

--- containers/hmmm/fetch_repo_and_collect_coverage.sh
+++ containers/hmmm/fetch_repo_and_collect_coverage.sh
@@ -5,9 +5,6 @@ set -e
 set -u
 set -o pipefail
 
-LOCAL_REPO_DESTINATION="./tdl-runner"
-LOCAL_SRCS_FILE="./file.srcs"
-
 # To check if an ENV variable exists, we dereference the input string $1 -> !1
 # then then we attempt parameter expansion with the "+x" string
 # it should return an empty string if ENV not defined
@@ -15,6 +12,8 @@ LOCAL_SRCS_FILE="./file.srcs"
 function ensure_env {
     if [ -z "${!1+x}" ]; then echo "Environment variable $1 not set"; exit 1; fi
 }
+
+ensure_env "WORK_DIR"
 
 ensure_env "S3_ENDPOINT"
 ensure_env "S3_REGION"
@@ -27,6 +26,10 @@ ensure_env "REPO"
 ensure_env "TAG"
 ensure_env "CHALLENGE_ID"
 
+# Configure local files
+LOCAL_REPO_DESTINATION="${WORK_DIR}/tdl-runner"
+LOCAL_SRCS_FILE="${WORK_DIR}/file.srcs"
+
 
 # Clone and switch to TAG
 if [[ "${REPO}" == s3://* ]];
@@ -36,19 +39,24 @@ then
             --region ${S3_REGION} \
             --no-verify-ssl \
             cp ${REPO} ${LOCAL_SRCS_FILE}
+    java -jar ${WORK_DIR}/dev-sourcecode-record-all.jar export \
+        --input ${LOCAL_SRCS_FILE} \
+        --output ${LOCAL_REPO_DESTINATION} \
+        --tag ${TAG}
 
 else
     echo "Assuming Git repo"
     git clone ${REPO} ${LOCAL_REPO_DESTINATION}
-    cd ${LOCAL_REPO_DESTINATION}
-    git checkout ${TAG}
-    git pull origin ${TAG}
+    git --git-dir ${LOCAL_REPO_DESTINATION} checkout ${TAG}
+    git --git-dir ${LOCAL_REPO_DESTINATION} pull origin ${TAG}
 fi
 
 
 # Run the coverage
+COVERAGE_SCRIPT="${LOCAL_REPO_DESTINATION}/get_coverage_for_challenge.sh"
+chmod a+x ${COVERAGE_SCRIPT}
 echo  "~~~~~~ START run external script ~~~~~~" > /dev/null
-./get_coverage_for_challenge.sh ${CHALLENGE_ID}
+${COVERAGE_SCRIPT} ${CHALLENGE_ID}
 echo "~~~~~~ STOP run external script ~~~~~~" > /dev/null
 
 # Publish to Queue
