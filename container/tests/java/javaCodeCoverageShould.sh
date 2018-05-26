155bec45c9f418102006f74ef0c25a2c8043a9f9
neomatrix369 <sadhak001@gmail.com>
2018-05-26 21:56:52 +0100

    Applying code review comments from @julian

--- container/tests/java/javaCodeCoverageShould.sh
+++ container/tests/java/javaCodeCoverageShould.sh
@@ -2,6 +2,7 @@
 
 computeCoverageForChallenge() {
    # given
+   SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
    language_id=$1
    tag=$2
    repo="https://github.com/julianghionoiu/tdl-runner-${language_id}"
@@ -9,36 +10,34 @@ computeCoverageForChallenge() {
    expectedResult="$4"
    participant_id="participant"
    round_id="round"
-   (cd ../.. && ./buildDockerImage.sh ${language_id})
-   exitCode=$?
-   if [[ ${exitCode} -ne 0 ]]; then
-      echo "Test failed during setup, due to failing to build docker image ${language_id} language"
-      echo "   Actual exit code: ${exitCode}"
-      echo "   Expected exit code: 0"
+
+   dockerImagePresent=$(docker images -q -f reference=accelerate-io/dpnt-coverage-${language_id}:latest)
+   if [[ -z "${dockerImagePresent}" ]] || [[ $? -ne 0 ]]; then
+      echo "Test failed during setup, due to failing to find docker image for ${language_id} language" 1>&2
    fi
 
    # when
-   actualResult=$( cd ../.. && ./runDockerContainer.sh ${language_id} ${participant_id} ${round_id} ${repo} ${tag} ${challenge_id} | tail -1 )
+   actualResult=$( . ${SCRIPT_CURRENT_DIR}/../../runDockerContainer.sh ${language_id} ${participant_id} ${round_id} ${repo} ${tag} ${challenge_id} | tail -1 )
 
    # then
    exitCode=$?
    if [[ ${exitCode} -eq 0 ]]; then
       echo "Test passed"
    else
-      echo "Test failed due to exit code mismatch"
-      echo "   Actual exit code: ${exitCode}"
-      echo "   Expected exit code: 0"
+      echo "Test failed due to exit code mismatch" 1>&2
+      echo "   Actual exit code: ${exitCode}"      1>&2
+      echo "   Expected exit code: 0"              1>&2
    fi
 
    if [[ "${actualResult}" = "${expectedResult}" ]]; then
       echo "Test passed"
    else
-      echo "Test failed due to result mismatch"
-      echo "   Actual result: '${actualResult}'"
-      echo "   Expected result: '${expectedResult}'"
+      echo "Test failed due to result mismatch"      1>&2
+      echo "   Actual result: '${actualResult}'"     1>&2
+      echo "   Expected result: '${expectedResult}'" 1>&2
    fi
 
 }
 
-computeCoverageForChallenge "java" "master" "SUM" "100"
-computeCoverageForChallenge "java" "master" "CHK" "0"
+computeCoverageForChallenge "java" "adjust-script-to-run-with-new-changes" "SUM" "100"
+computeCoverageForChallenge "java" "adjust-script-to-run-with-new-changes" "CHK" "0"
