1c788892504578f35fdde4a612136f2b669dcf2d
neomatrix369 <sadhak001@gmail.com>
2018-05-26 22:37:04 +0100

    Adding test passed/failed summary after test execution

--- container/tests/java/javaCodeCoverageShould.sh
+++ container/tests/java/javaCodeCoverageShould.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+passedTests=()
+failedTests=()
+
 computeCoverageForChallenge() {
    # given
    SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -14,6 +17,8 @@ computeCoverageForChallenge() {
    dockerImagePresent=$(docker images -q -f reference=accelerate-io/dpnt-coverage-${language_id}:latest)
    if [[ -z "${dockerImagePresent}" ]] || [[ $? -ne 0 ]]; then
       echo "Test failed during setup, due to failing to find docker image for ${language_id} language" 1>&2
+      testOutcome "Failed" "${language_id}" "${challenge_id}" "${participant_id}" "${round_id}"
+      return
    fi
 
    # when
@@ -31,13 +36,49 @@ computeCoverageForChallenge() {
 
    if [[ "${actualResult}" = "${expectedResult}" ]]; then
       echo "Test passed"
+      testOutcome "Passed" "${language_id}" "${challenge_id}" "${participant_id}" "${round_id}"
    else
       echo "Test failed due to result mismatch"      1>&2
       echo "   Actual result: '${actualResult}'"     1>&2
       echo "   Expected result: '${expectedResult}'" 1>&2
+      testOutcome "Failed" "${language_id}" "${challenge_id}" "${participant_id}" "${round_id}"
    fi
+}
 
+testOutcome() {
+    outcome="$1"
+    language_id="$2"
+    challenge_id="$3"
+    participant_id="$4"
+    round_id="$5"
+
+    if [[ "${outcome}" = "Passed" ]]; then
+        passedTests+=("${language_id}|${challenge_id}|${participant_id}|${round_id}")
+    elif [[ "${outcome}" = "Failed" ]]; then
+        failedTests+=("${language_id}|${challenge_id}|${participant_id}|${round_id}")
+    fi
+}
+
+displayPassFailSummary(){
+    echo ""
+    echo "~~~ Summary of test executions ~~~"
+    echo "  ~~~ Passed Tests ~~~"
+    for passedTest in ${passedTests[@]}
+    do
+        echo "  ${passedTest}"
+    done
+    echo "  ${#passedTests[@]} test(s) passed"
+
+    echo ""
+    echo "  ~~~ Failed Tests ~~~"
+    for failedTest in ${failedTests[@]}
+    do
+        echo "  ${failedTest}"
+    done
+    echo "  ${#failedTests[@]} test(s) failed"
 }
 
 computeCoverageForChallenge "java" "adjust-script-to-run-with-new-changes" "SUM" "100"
 computeCoverageForChallenge "java" "adjust-script-to-run-with-new-changes" "CHK" "0"
+
+displayPassFailSummary
