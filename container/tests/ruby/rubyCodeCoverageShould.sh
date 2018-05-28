51b87057353372045c7c657b38b11aeec6d77ecd
neomatrix369 <sadhak001@gmail.com>
2018-05-28 23:29:05 +0100

    Ruby: created script to run the test for getting the coverage information for SUM & CHK challenges in Ruby

---
+++ container/tests/ruby/rubyCodeCoverageShould.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source ${SCRIPT_CURRENT_DIR}/../test-framework/test-coverage-functions.sh
+
+computeCoverageForChallenge "ruby" "master" "SUM" "100"
+computeCoverageForChallenge "ruby" "master" "CHK" "0"
+
+displayPassFailSummary
