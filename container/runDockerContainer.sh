ae9f56ff0b286a31e6ef9fe9957b22f1201480ba
neomatrix369 <sadhak001@gmail.com>
2018-06-06 15:26:28 +0100

    Docker: merge the debugDockerContainer.sh into runDockerContainer.sh, provide a flag to trigger the debug mode

--- container/runDockerContainer.sh
+++ container/runDockerContainer.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMAGES_DIR="${SCRIPT_CURRENT_DIR}/images"
+export DEBUG="${DEBUG:-}"
 
 function die() { echo >&2 $1; exit 1; }
 [ "$#" -eq 6 ] || die "Usage: $0 LANGUAGE_ID PARTICIPANT_ID ROUND_ID REPO TAG CHALLENGE_ID"
@@ -23,17 +24,41 @@ language_image_name="${DEFAULT_IMAGE_PREFIX}${LANGUAGE_ID}"
 language_image_tag="${language_image_name}:${language_image_version}"
 
 echo "Running ${language_image_tag} from the local docker registry"
-docker run                                                                      \
-      --env AWS_ACCESS_KEY_ID=unused                                            \
-      --env AWS_SECRET_KEY=unused                                               \
-      --env S3_ENDPOINT=unused                                                  \
-      --env S3_REGION=unused                                                    \
-      --env SQS_ENDPOINT=unused                                                 \
-      --env SQS_REGION=unused                                                   \
-      --env SQS_QUEUE_URL=unused                                                \
-      --env PARTICIPANT_ID=${PARTICIPANT_ID}                                    \
-      --env ROUND_ID=${ROUND_ID}                                                \
-      --env REPO=${REPO}                                                        \
-      --env TAG=${TAG}                                                          \
-      --env CHALLENGE_ID=${CHALLENGE_ID}                                        \
-      ${language_image_tag}
+if [[ "${DEBUG}" == "true" ]]; then
+    echo "*************************"
+    echo "* Running in Debug mode *"
+    echo "*************************"
+    docker run                                                                      \
+          --interactive                                                             \
+          --tty                                                                     \
+          --entrypoint "/bin/bash"                                                  \
+          --env AWS_ACCESS_KEY_ID=unused                                            \
+          --env AWS_SECRET_KEY=unused                                               \
+          --env S3_ENDPOINT=unused                                                  \
+          --env S3_REGION=unused                                                    \
+          --env SQS_ENDPOINT=unused                                                 \
+          --env SQS_REGION=unused                                                   \
+          --env SQS_QUEUE_URL=unused                                                \
+          --env PARTICIPANT_ID=${PARTICIPANT_ID}                                    \
+          --env ROUND_ID=${ROUND_ID}                                                \
+          --env REPO=${REPO}                                                        \
+          --env TAG=${TAG}                                                          \
+          --env CHALLENGE_ID=${CHALLENGE_ID}                                        \
+          ${language_image_tag}
+
+else
+    docker run                                                                      \
+          --env AWS_ACCESS_KEY_ID=unused                                            \
+          --env AWS_SECRET_KEY=unused                                               \
+          --env S3_ENDPOINT=unused                                                  \
+          --env S3_REGION=unused                                                    \
+          --env SQS_ENDPOINT=unused                                                 \
+          --env SQS_REGION=unused                                                   \
+          --env SQS_QUEUE_URL=unused                                                \
+          --env PARTICIPANT_ID=${PARTICIPANT_ID}                                    \
+          --env ROUND_ID=${ROUND_ID}                                                \
+          --env REPO=${REPO}                                                        \
+          --env TAG=${TAG}                                                          \
+          --env CHALLENGE_ID=${CHALLENGE_ID}                                        \
+          ${language_image_tag}
+fi
