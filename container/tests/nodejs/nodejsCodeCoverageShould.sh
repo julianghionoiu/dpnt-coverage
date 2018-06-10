7f3e11b5f5d7fea09fbed94f0eec7c62082dfefb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-10 22:35:37 +0100

    Update the nodejs container and add precaching

--- container/tests/nodejs/nodejsCodeCoverageShould.sh
+++ container/tests/nodejs/nodejsCodeCoverageShould.sh
@@ -7,6 +7,8 @@ SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SCRIPT_CURRENT_DIR}/../test-framework/test-coverage-functions.sh
 
 computeCoverageForChallenge "nodejs" "master" "SUM" "100"
+computeCoverageForChallenge "nodejs" "master" "TST" "75"
 computeCoverageForChallenge "nodejs" "master" "CHK" "0"
+computeCoverageForChallenge "nodejs" "master" "xyz" "0"
 
 displayPassFailSummary
