2d672206c85eefda5d61b73d3f445dca3f51b6a5
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-10 22:50:23 +0100

    Add Python failing tests

--- container/tests/python/pythonCodeCoverageShould.sh
+++ container/tests/python/pythonCodeCoverageShould.sh
@@ -7,6 +7,8 @@ SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SCRIPT_CURRENT_DIR}/../test-framework/test-coverage-functions.sh
 
 computeCoverageForChallenge "python" "master" "SUM" "100"
+computeCoverageForChallenge "python" "master" "TST" "50"
 computeCoverageForChallenge "python" "master" "CHK" "0"
+computeCoverageForChallenge "python" "master" "xyz" "0"
 
 displayPassFailSummary
