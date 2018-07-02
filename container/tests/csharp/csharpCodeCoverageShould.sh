35d1d2dc16d51ccdd56e83217aac921f5020a839
neomatrix369 <sadhak001@gmail.com>
2018-07-02 16:47:04 +0100

    Rename all dotnet terms to csharp all across the project

---
+++ container/tests/csharp/csharpCodeCoverageShould.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source ${SCRIPT_CURRENT_DIR}/../test-framework/test-coverage-functions.sh
+
+computeCoverageForChallenge "csharp" "master" "SUM" "100"
+computeCoverageForChallenge "csharp" "master" "TST" "50"
+computeCoverageForChallenge "csharp" "master" "CHK" "0"
+computeCoverageForChallenge "csharp" "master" "xyz" "0"
+
+displayPassFailSummary
