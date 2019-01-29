05cf6dddb18eb1526fea23cd44aaa3fa0e3b9d94
neomatrix369 <sadhak001@gmail.com>
2019-01-29 19:53:58 +0000

    Applied negative test check to vbnet container, tests are green

--- container/tests/vbnet/vbnetCodeCoverageShould.sh
+++ container/tests/vbnet/vbnetCodeCoverageShould.sh
@@ -9,6 +9,7 @@ source ${SCRIPT_CURRENT_DIR}/../test-framework/test-coverage-functions.sh
 computeCoverageForChallenge "vbnet" "master" "SUM" "100"
 computeCoverageForChallenge "vbnet" "master" "TST" "50"
 computeCoverageForChallenge "vbnet" "master" "CHK" "0"
-computeCoverageForChallenge "vbnet" "master" "xyz" "0"
+
+checkForFailingCoverageResults "vbnet" "master" "xyz" ""
 
 displayPassFailSummary
